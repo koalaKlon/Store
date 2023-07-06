4792cfe6483ec87935da08cd1f72788f6feded44
KoalaKlon <koala.klon@gmail.com>
2023-07-06 17:22:01 +0300

    First load to the new git repository

--- clothing/urls.py
+++ clothing/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('basket', views.product, name='product'),
     path('register', views.register_request, name='register'),
     path('login', views.login_request, name='login'),
+    path('logout', views.logout_request, name='logout'),
 ]
 
--- clothing/views.py
+++ clothing/views.py
@@ -6,7 +6,7 @@ from django.views.generic import CreateView
 from .forms import UserRegistrationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm #add this
 
 
@@ -56,4 +56,12 @@ def register_request(request):
             return render(request=request, template_name="clothing/register.html", context={"register_form": form})
     form = UserRegistrationForm()
     return render(request=request, template_name="clothing/register.html", context={"register_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out. ")
+    return redirect("home")
+
+
 
